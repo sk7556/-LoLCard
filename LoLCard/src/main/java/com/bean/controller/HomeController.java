3d06489ccc2ccef934e01ba4e5935e361242bb24
Jungsik-Nam <kuuko7556@gmail.com>
2020-06-28 00:00:00 +0900

    실행가능한롤카드재업로드

---
+++ LoLCard/src/main/java/com/bean/controller/HomeController.java
@@ -0,0 +1,52 @@
+package com.bean.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bean.config.VersionCheck;
+import com.bean.dao.SearchLogDao;
+import com.bean.dto.Summoner;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		SearchLogDao dao = new SearchLogDao();
+		List<Summoner> list = dao.getSearchLogs();
+		VersionCheck.checkVersion();
+		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("name", "이창섭");
+		model.addAttribute("idList", list);
+		model.addAttribute("version", VersionCheck.profileiconVersion);
+		return "home";
+	}
+	
+	
+	
+
+
+
+}
---
+++ LoLCard/src/main/java/com/bean/controller/SearchSummoner.java
@@ -0,0 +1,120 @@
+package com.bean.controller;
+
+import static com.bean.dao.ApiKeyValue.API_KEY;
+
+import java.io.BufferedReader;	
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bean.dao.SearchLogDao;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.bean.config.VersionCheck;
+import com.bean.dto.LeagueEntrydto;
+import com.bean.dto.Summoner;;
+
+@Controller
+public class SearchSummoner {
+
+	@RequestMapping(value="/search", method=RequestMethod.GET)
+	public String searchSummoner(Model model, HttpServletRequest httpServletRequest) {
+		VersionCheck.checkVersion();
+		BufferedReader br = null;
+		String SummonerName = httpServletRequest.getParameter("title");
+		Summoner temp= null;
+		LeagueEntrydto[] leagueInfo = null;
+		try{            
+			String urlstr = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+
+					SummonerName.replace(" ", "")		+"?api_key="+API_KEY;
+			URL url = new URL(urlstr);
+			HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+			urlconnection.setRequestMethod("GET");
+			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8")); // 여기에 문자열을 받아와라.
+			String result = "";
+			String line;
+			while((line = br.readLine()) != null) { // 그 받아온 문자열을 계속 br에서 줄단위로 받고 출력하겠다.
+				result = result + line;
+			}
+			JsonParser jsonParser = new JsonParser();
+			JsonObject k = (JsonObject) jsonParser.parse(result);
+			int profileIconId = k.get("profileIconId").getAsInt();
+			String name = k.get("name").getAsString();
+			String puuid = k.get("puuid").getAsString();
+			long summonerLevel = k.get("summonerLevel").getAsLong();
+			long revisionDate = k.get("revisionDate").getAsLong();
+			String id = k.get("id").getAsString();
+			String accountId = k.get("accountId").getAsString();
+			temp = new Summoner(profileIconId,name,puuid,summonerLevel,revisionDate,id,accountId);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		SearchLogDao dao = new SearchLogDao();
+		dao.addSearchLog(temp);
+		String[] leagueName = null;
+		try{            
+			String urlstr = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/"+
+					temp.getId()		+"?api_key="+API_KEY;
+			URL url = new URL(urlstr);
+			HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+			urlconnection.setRequestMethod("GET");
+			br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8")); // 여기에 문자열을 받아와라.
+			String result = "";
+			String line;
+			while((line = br.readLine()) != null) { // 그 받아온 문자열을 계속 br에서 줄단위로 받고 출력하겠다.
+				result = result + line;
+			}
+			JsonParser jsonParser = new JsonParser();
+			JsonArray arr = (JsonArray) jsonParser.parse(result);
+			leagueInfo = new LeagueEntrydto[arr.size()];
+			leagueName = new String[arr.size()];
+			for(int i=0; i<arr.size(); i++) {
+				JsonObject k =  (JsonObject) arr.get(i);
+				int wins = k.get("wins").getAsInt();
+				int losses = k.get("losses").getAsInt();
+				String rank = k.get("rank").getAsString();
+				String tier = k.get("tier").getAsString();
+				String queueType = k.get("queueType").getAsString();
+				int leaguePoints = k.get("leaguePoints").getAsInt();
+				String leagueId = k.get("leagueId").getAsString();
+				leagueInfo[i] = new LeagueEntrydto(queueType, wins, losses, leagueId, rank,tier, leaguePoints);
+				urlstr = "https://kr.api.riotgames.com/lol/league/v4/leagues/"+
+						leagueInfo[i].getLeagueId()		+"?api_key="+API_KEY;
+				url = new URL(urlstr);
+				urlconnection = (HttpURLConnection) url.openConnection();
+				urlconnection.setRequestMethod("GET");
+				br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8")); // 여기에 문자열을 받아와라.
+				result = "";
+				line ="";
+				while((line = br.readLine()) != null) { // 그 받아온 문자열을 계속 br에서 줄단위로 받고 출력하겠다.
+					result = result + line;
+				}
+				jsonParser = new JsonParser();
+				k = (JsonObject) jsonParser.parse(result);
+				leagueName[i] = k.get("name").getAsString();
+			}
+			
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		System.out.print(leagueInfo[0]);
+		model.addAttribute("summoner", temp);
+		model.addAttribute("profileImgURL", "http://ddragon.leagueoflegends.com/cdn/"+VersionCheck.profileiconVersion+"/img/profileicon/"+temp.getProfileIconId()+".png");
+		model.addAttribute("leagueInfo", leagueInfo);
+		model.addAttribute("tierImgURL", "img/emblems/Emblem_"+leagueInfo[0].getTier()+".png");
+		model.addAttribute("leagueName", leagueName);
+		return "result";
+	}
+	
+}
