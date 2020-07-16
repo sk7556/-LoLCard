b71e5fa5f54e3c5d9bbe656195cf28df2e2b0e37
Jungsik-Nam <kuuko7556@gmail.com>
2020-07-16 18:23:33 +0900

    jgjh

--- LoLCard/src/main/java/com/bean/controller/MatchController.java
+++ LoLCard/src/main/java/com/bean/controller/MatchController.java
@@ -4,13 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
